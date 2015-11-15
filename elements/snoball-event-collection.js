3f97289e75139c1ab3f2078849d3a48a227e746b
rbransby <rob@bransby.com.au>
2015-11-15 16:24:56 +0800

    wiring up events

--- elements/snoball-event-collection.js
+++ elements/snoball-event-collection.js
@@ -11,16 +11,10 @@
       }
       created() {}
       ready() {
-        this.items = this.populateWithTestData();        
+        //this.items = this.populateWithTestData();        
       }
       attached() {}
       detached() {}
       attributeChanged() {}
-      populateWithTestData()
-      {
-        return [
-          { eventType : 'snoball-chat', payload : 'test chat message'},
-          { eventType : 'snoball-new-game', payload : {Numbers : [1,2,3,4,5,6], Target: 758}},          
-        ];
-      }
+      
     }
--- webapp.js
+++ webapp.js
@@ -2,24 +2,32 @@
   'use strict';
   var socket = io();
   
+  var app = document.querySelector('#webapp');
+  var snoballEventCollection;
+  
+  app.snoballEvents = [
+          { eventType : 'snoball-chat', payload : 'test chat message'},
+          { eventType : 'snoball-new-game', payload : {Numbers : [1,2,3,4,5,6], Target: 758}},          
+        ];
+  
   socket.on('snoball-chat', (msg) => {
-    
+    app.push('snoballEvents',{eventType: 'snoball-chat', payload: msg});
   });
   
   socket.on('snoball-player-joined', (playerName) => {
-    
+    app.push('snoballEvents',{eventType: 'snoball-player-joined', payload: playerName});
   });
   
   socket.on('snoball-new-game', (snoballGame) => {
-    
+    app.push('snoballEvents',{eventType: 'snoball-new-game', payload: snoballGame});
   });
   
   //solution = {playerName : 'playerName'}
   socket.on('snoball-solution-received', (solution) => {
-    
+    app.push('snoballEvents',{eventType: 'snoball-solution-received', payload: solution});
   });
   
-  var app = document.querySelector('#webapp');
+  
   app.addEventListener('dom-change', function() {	 
     console.log('dom change fired');     
     document.querySelector('#snoballInput').addEventListener('snoball-command-received', (eventArgs) => {
