26bc91e0f535b74b762695d5d00984a35079459a
rbransby <rob@bransby.com.au>
2015-11-16 21:20:51 +0800

    added integ test

--- test/snoball-server.test.js
+++ test/snoball-server.test.js
@@ -1,4 +1,22 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io-client');
+
+var socketURL = 'http://localhost:3000';
+
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+describe('Snoball Server @integ', () => {
+  it('should announce when a player joins', (done) => {
+    var client1 = io.connect(socketURL, options);
+    client1.on('connect', function (data) {
+      client1.emit('snoball-join-game', 'TestPlayer');
+    });
+
+    client1.on('snoball-player-joined', (name) => {
+      name.should.equal('TestPlayer');
+      done();
+    });
+  });
+});
